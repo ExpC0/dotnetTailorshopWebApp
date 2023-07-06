5f5122e12d35ed07de0db79176b0a17e7564d7af
Ryan <exp@chamber1.ryn>
2023-07-06 22:05:32 +0600

    uploading Asp.net webApp

---
+++ tailorshop5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tailorshop5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
